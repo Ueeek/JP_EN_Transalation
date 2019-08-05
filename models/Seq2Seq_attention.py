7f5f45607a4e0b11bef5c4ecf1f51f1e89469330
Ueki <ueki@UekinoMacBook-Air.local>
2019-08-05 22:00:16 +0900

    fix loss

--- models/Seq2Seq_attention.py
+++ models/Seq2Seq_attention.py
@@ -232,6 +232,7 @@ class Seq2Seq:
                 batch_x = batch_x.to(device)
                 batch_y = batch_y.to(device)
                 val_loss += self.validation(batch_x, batch_y)
+            print("loss->{}, val_loss->{}".format(epoch_loss, val_loss))
             print("epoch{}. time->{}".format(epoch+1, time.time()-start))
             print("loss->{}, val_loss->{}".format(epoch_loss/batch_cnt, val_loss/val_bacth))
             print("----------------")
